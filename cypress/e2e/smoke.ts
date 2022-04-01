f5972578ba201be0f1651408fec49ccc53e5529e
Scott Smerchek <scott.smerchek@gmail.com>
2022-04-01 14:58:46 -0500

    fix cypress test

--- cypress/e2e/smoke.ts
+++ cypress/e2e/smoke.ts
@@ -2,6 +2,6 @@ describe('smoke tests', () => {
   it('should allow you to visit a blog post', () => {
     cy.visit('/blog/taking-the-d-out-of-crud-with-soft-updates');
 
-    cy.findByText("Taking the 'D' Out of CRUD with Soft Updates").should('exist');
+    cy.get('h1').should('contain', 'CRUD with Soft Updates');
   });
 });
